3658db1f0e14e3a6e3827694ddc3207f9c3a98ff
Jorg Are <jorg2all@yahoo.com>
2017-02-24 13:01:58 +0100

    feature(api-endpoints): use environment variables for database config

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,9 @@
 import fs from 'fs';
 import path from 'path';
 import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const basename = path.basename(module.filename);
 const env = process.env.NODE_ENV || 'development';
@@ -11,7 +14,7 @@ const db = {};
 let sequelize;
 
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(
     config.database, config.username, config.password, config

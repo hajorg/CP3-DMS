14aa83523845431b7074a4f9c0d36673b3ca2a1b
Jorg Are <jorg2all@yahoo.com>
2017-03-09 09:46:18 +0100

    refactor(pagination): refactor limit and offset

--- server/app/helper/utility.js
+++ server/app/helper/utility.js
@@ -15,13 +15,21 @@ const Utility = {
         message: this.limitOffsetMessage('limit', 1)
       });
     }
+
     if (req.query.offset < 0) {
       return res.status(400).send({
         message: 'Please enter a valid number starting from 0 for offset.'
       });
     }
+
     req.query.limit = req.query.limit ? +req.query.limit : 10;
     req.query.offset = req.query.offset ? +req.query.offset : 0;
+
+    if (!/^\d+$/.test(req.query.offset) || !/^\d+$/.test(req.query.limit)) {
+      return res.status(400).send({
+        message: 'Please enter a valid number for both offset and limit.'
+      });
+    }
     return true;
   },
 

b0c51ce7ed5d7cda32532aaeb4f3dfdcff9aefa0
Jorg Are <jorg2all@yahoo.com>
2017-04-30 12:08:42 +0100

    fix(server): add access control

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
 const httpServer = http.createServer(app);
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Access-Control-Allow-Credentials');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  next();
+});
+
 // Endpoints route
 app.use('/', routes.userRoutes);
 app.use('/', routes.documentRoutes);
